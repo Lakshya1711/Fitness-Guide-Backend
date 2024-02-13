647743169720a25afce34932f7b10e4d46fbeeaf
Lakshya1711 <Lucky17sh@gmail.com>
2024-02-13 19:37:25 +0530

    User Connected

--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -141,3 +141,16 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
